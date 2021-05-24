babfd30d846adfaa0b7d6a52802edb182eb53a08
rukysandy <rukysandy57@gmail.com>
2021-05-24 16:06:21 +0100

    removed gatsby node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,15 +1,23 @@
-exports.onCreateBabelConfig = ({ babelrc }) => ({
-  ...babelrc,
-  ...(process.env.NODE_ENV !== "development" && {
-    plugins: babelrc.plugins.concat([
-      "transform-regenerator",
-      "transform-runtime",
-    ]),
-  }),
-})
-exports.onCreateWebpackConfig = ({ config, stage }) => {
-  if (stage === "build-javascript") {
-    config._config.entry.app = ["babel-polyfill", config._config.entry.app]
-  }
-  return config
-}
+// exports.onCreateBabelConfig = ({ babelrc }) => ({
+
+//   ...babelrc,
+//   ...(process.env.NODE_ENV !== "development" && {
+//     plugins: babelrc.plugins.concat([
+//       "transform-regenerator",
+//       "transform-runtime",
+//     ]),
+//   }),
+// })
+
+// exports.onCreateBabelConfig = ({ actions }) => {
+//   actions.setBabelPlugin({
+//     name: `babel-plugin-foo`,
+//   })
+
+//   exports.onCreateWebpackConfig = ({ config, stage }) => {
+//     if (stage === "build-javascript") {
+//       config._config.entry.app = ["babel-polyfill", config._config.entry.app]
+//     }
+//     return config
+//   }
+// }
